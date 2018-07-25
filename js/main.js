5f6879617f7ed7ecdc0fc92d3388838c7ff10cc8
Slartbartfast1 <38152942+Slartbartfast1@users.noreply.github.com>
2018-07-25 22:54:07 +0800

    first commit

---
+++ js/main.js
@@ -0,0 +1,58 @@
+
+$(function(){
+    let i=0;
+    let size=5;
+    //     //小圆点开始
+    $(".dot").click(function(){
+        $(this).addClass("on").siblings().removeClass("on");
+        let index=$(this).index();
+        i = index;//将小圆点索引赋值给轮播图索引
+        $(".slider-list").stop().animate({ left: -index * 600 }, 600);
+    });
+    //小圆点结束
+
+    //定时器开始
+    let t = setInterval(function () { i++; move();},2000);
+    $(".slider-box").hover(function () {
+        clearInterval(t);
+    }, function () {
+        t = setInterval(function () { i++; move(); }, 2000);
+    });
+    //定时器结束
+
+    //前后按钮开始
+    $(".next").click(function () {
+        i++;
+        move();
+    });
+    $(".prev").click(function () {
+        i--;
+        move();
+    });
+    //前后按钮结束
+
+
+    //轮播图移动开始
+    function move(){
+        if (i==5){
+            $('.slider-list').css({left:0});
+            i=1;
+        }
+        // if(i=-1){
+        //     $(".main").css({left:-3*600});
+        //     i=3;
+        // }
+        if(i==-1){
+            $(".slider-list").css({left:-4*600});
+            i=3;
+        }
+        $(".slider-list").stop().animate({ left: -i * 600 }, 600);
+        // i++;
+        if (i == size - 1) {
+            $(".dot").eq(0).addClass("on").siblings().removeClass("on");
+        } else {
+            $(".dot").eq(i).addClass("on").siblings().removeClass("on");
+        }
+    }
+});
+//轮播图移动结束
